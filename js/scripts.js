e8c64f90d29d7ee7a6700c5416cc1d361d7e712f
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-09 11:22:19 -0700

    Fix bug in form .onsubmit; incorrect form ID used

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+// Biz Logic
 function mrReplace(userNum) {
     const wordsToMR = [];
 
@@ -18,5 +18,48 @@ function mrReplace(userNum) {
     return wordsToMR.join(", ");
 }
 
-const userNum = 14;
-console.log(mrReplace(userNum));
+// const userNum = 14;
+// console.log(mrReplace(userNum));
+
+// UI Logic
+
+window.addEventListener("load", function() {
+    document.querySelector("#num-replacer").onsubmit = function(event) {
+        event.preventDefault();
+
+        let numInput = document.getElementById("number").value;
+
+        console.log(numInput);
+
+        resetForm();
+    };
+});
+
+function resetForm() {
+    document.getElementById("number").value = "";
+};
+
+
+
+// // Biz Logic
+// function mrReplace(userNum) {
+//     const wordsToMR = [];
+
+//     for (let i = 0; i <= userNum; i++) {
+//         let numStr = i.toString();
+//         if (numStr.includes("3")) {
+//             wordsToMR.push("Won't you be my neighbor?");
+//         } else if (numStr.includes("2")) {
+//             wordsToMR.push("Boop!");
+//         } else if (numStr.includes("1")) {
+//             wordsToMR.push("Beep!");
+//         } else {
+//             wordsToMR.push(i);
+//         }
+//     }
+
+//     return wordsToMR.join(", ");
+// }
+
+// const userNum = 14;
+// console.log(mrReplace(userNum));
