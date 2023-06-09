40f46de8cea25069d8c4c8a5f5e924861d0526af
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-09 11:30:52 -0700

    Get correct output at console.log(result)

--- js/scripts.js
+++ js/scripts.js
@@ -18,18 +18,15 @@ function mrReplace(userNum) {
     return wordsToMR.join(", ");
 }
 
-// const userNum = 14;
-// console.log(mrReplace(userNum));
-
 // UI Logic
-
 window.addEventListener("load", function() {
     document.querySelector("#num-replacer").onsubmit = function(event) {
         event.preventDefault();
 
         let numInput = document.getElementById("number").value;
+        let result = mrReplace(Number(numInput));
 
-        console.log(numInput);
+        console.log(result);
 
         resetForm();
     };
@@ -37,29 +34,4 @@ window.addEventListener("load", function() {
 
 function resetForm() {
     document.getElementById("number").value = "";
-};
-
-
-
-// // Biz Logic
-// function mrReplace(userNum) {
-//     const wordsToMR = [];
-
-//     for (let i = 0; i <= userNum; i++) {
-//         let numStr = i.toString();
-//         if (numStr.includes("3")) {
-//             wordsToMR.push("Won't you be my neighbor?");
-//         } else if (numStr.includes("2")) {
-//             wordsToMR.push("Boop!");
-//         } else if (numStr.includes("1")) {
-//             wordsToMR.push("Beep!");
-//         } else {
-//             wordsToMR.push(i);
-//         }
-//     }
-
-//     return wordsToMR.join(", ");
-// }
-
-// const userNum = 14;
-// console.log(mrReplace(userNum));
+}
